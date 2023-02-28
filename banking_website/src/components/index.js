5d1d5d31ab7273e3d023c2b7126487b5c916e48c
Luke <laj.baxter@gmail.com>
2023-02-28 14:47:47 +1100

    here_we_go

---
+++ banking_website/src/components/index.js
@@ -0,0 +1,25 @@
+import Billing from "./billing";
+import Business from "./business";
+import Button from "./button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Hero,
+    Stats,
+    Testimonials,
+    Navbar,
+};
